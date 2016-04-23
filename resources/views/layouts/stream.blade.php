cbb2b7bde433fb18e12b5b820069337a8d477924
David Ives <david@crowdcube.com>
2016-04-24 00:30:10 +0100

    CSS manipulation made more automatic

--- resources/views/layouts/stream.blade.php
+++ resources/views/layouts/stream.blade.php
@@ -24,15 +24,35 @@
 @section('footer')
     <script>
         var socket = io('http://192.168.10.10:3000');
+
+        var typeMeta = {
+            funded: {
+                panel: 'success',
+                icon: 'ok'
+            },
+            investment: {
+                'panel': 'warning',
+                'icon': 'piggy-bank'
+            },
+            birthday: {
+                'panel': 'info',
+                'icon': 'user'
+            },
+            d: {
+                'panel': 'success',
+                'icon': 'bullhorn'
+            }
+        };
+
         socket.on("town-crier:App\\Events\\Shout", function(message) {
 
             if (message.data !== null) {
 
-                var data = message.data.announcement;
-                var type = resolveShoutClass(data.type);
+                var data = message.data.announcement,
+                    type = typeMeta[data.type]
 
                 var panel = $('<li class="list-group-item list-group-item-'+type.panel+'">'+
-                        '<h4 class="shout-title"><span class="glyphicon shout-type"></span>'+data.title+'</h4>'+
+                        '<h4 class="shout-title"><span class="glyphicon glyphicon-'+type.icon+' shout-type"></span>'+data.title+'</h4>'+
                         '<div>'+
                         data.body+
                         '</div>'+
@@ -41,54 +61,25 @@
 
                 // Display the latest Shout
                 $('.shouts').prepend(panel);
-
-                console.log(panel);
             }
 
         });
-
-        var resolveShoutClass = function(type) {
-
-            switch (type) {
-                case 'funded':
-                    return {
-                        'panel': 'success',
-                        'icon': 'ok',
-                    };
-                case 'birthday':
-                    return {
-                        'panel': 'info',
-                        'icon': 'user',
-                    };
-                case 'investment':
-                    return {
-                        'panel': 'warning',
-                        'icon': 'piggy-bank',
-                    };
-                default:
-                    return {
-                        'panel': 'default',
-                        'icon': 'bullhorn',
-                    };
-            }
-        }
 
         $(document).ready(function() {
 
             // For each of the following elements that are found,
             // update the outlying element to swap out the type for a bootstrap style
             // and find the icon and swap out for the correct version
-            $('.funded').addClass('list-group-item-'+resolveShoutClass('funded').panel)
+            // TODO - this needs to loop the typeMeta array
+            $('.funded').addClass('list-group-item-' + typeMeta.funded.panel)
                     .find('.shout-type')
-                    .addClass('glyphicon-'+resolveShoutClass('funded').icon);
-            $('.birthday').addClass('list-group-item-'+resolveShoutClass('birthday').panel)
+                    .addClass('glyphicon-' + typeMeta.funded.icon);
+            $('.birthday').addClass('list-group-item-' + typeMeta.birthday.panel)
                     .find('.shout-type')
-                    .addClass('glyphicon-'+resolveShoutClass('birthday').icon);
-            $('.investment').addClass('list-group-item-'+resolveShoutClass('investment').panel)
+                    .addClass('glyphicon-' + typeMeta.birthday.icon);
+            $('.investment').addClass('list-group-item-' + typeMeta.investment.panel)
                     .find('.shout-type')
-                    .addClass('glyphicon-'+resolveShoutClass('investment').icon);
-//            $('.birthday').addClass('list-group-item-info');
-//            $('.investment').addClass('list-group-item-warning');
+                    .addClass('glyphicon-' + typeMeta.investment.icon);
         });
     </script>
 @stop
