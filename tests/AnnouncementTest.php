50ec5d71831fa0ee18d81b125d3c52081521dac9
David Ives <david@crowdcube.com>
2016-06-17 07:51:32 +0100

    Rationalised attribute creation with helper tests in test base

--- tests/AnnouncementTest.php
+++ tests/AnnouncementTest.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\User;
 use App\Announcement;
 use App\AnnouncementType;
-use App\User;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -26,14 +26,7 @@ class AnnouncementTest extends TestBase
      */
     public function funding_is_correctly_stored()
     {
-        $faker = Faker\Factory::create();
-
-        $attributes = [
-            'title'     => 'Pitch Funded',
-            'body'      => $faker->company().' has reached target!',
-            'type_id'   => AnnouncementType::FUNDED_ID,
-            'user_id'   => User::SYSTEM_USER_ID
-        ];
+        $attributes = $this->createFundingAttributes();
 
         Announcement::create($attributes);
 
@@ -45,16 +38,7 @@ class AnnouncementTest extends TestBase
      */
     public function funding_milestone_is_correctly_stored()
     {
-        $faker = Faker\Factory::create();
-
-        $milestones = [25, 50, 75, 90, 99];
-
-        $attributes = [
-            'title'     => $faker->company(),
-            'body'      => 'Milestone reached : '.$milestones[array_rand($milestones)].'% of target!',
-            'type_id'   => AnnouncementType::FUNDING_MILESTONE_ID,
-            'user_id'   => User::SYSTEM_USER_ID
-        ];
+        $attributes = $this->createFundingMilestoneAttributes();
 
         Announcement::create($attributes);
 
@@ -66,16 +50,7 @@ class AnnouncementTest extends TestBase
      */
     public function birthday_is_correctly_stored()
     {
-        $faker = Faker\Factory::create();
-
-        $user = $this->createUser();
-
-        $attributes = [
-            'title'     => 'Happy Birthday!',
-            'body'      => "It's ".$faker->name."'s Birthday today!",
-            'type_id'   => AnnouncementType::BIRTHDAY_ID,
-            'user_id'   => $user->id
-        ];
+        $attributes = $this->createBirthdayAttributes();
 
         Announcement::create($attributes);
 
@@ -87,14 +62,7 @@ class AnnouncementTest extends TestBase
      */
     public function code_deploy_is_correctly_stored()
     {
-        $user = $this->createSystemUser();
-
-        $attributes = [
-            'title'     => 'Blast Off!',
-            'body'      => 'New application code successfully deployed',
-            'type_id'   => AnnouncementType::CODE_DEPLOY_ID,
-            'user_id'   => $user->id
-        ];
+        $attributes = $this->createCodeDeployAttributes();
 
         Announcement::create($attributes);
 
@@ -106,14 +74,7 @@ class AnnouncementTest extends TestBase
      */
     public function announcement_is_correctly_stored()
     {
-        $user = $this->createUser();
-
-        $attributes = [
-            'title'     => 'Announcement title',
-            'body'      => 'Body of the announcement',
-            'type_id'   => AnnouncementType::ANNOUNCEMENT_ID,
-            'user_id'   => $user->id
-        ];
+        $attributes = $this->createAnnouncementAttributes();
 
         Announcement::create($attributes);
 
--- tests/TestBase.php
+++ tests/TestBase.php
@@ -59,13 +59,76 @@ class TestBase extends TestCase
     {
         $faker = Faker\Factory::create();
 
-        $attributes = [
+        return [
             'title'     => $faker->company(),
             'body'      => "£".$faker->numberBetween(10, 5000)." investment. Pitch has now reached ".$faker->numberBetween(5, 99)."%",
             'type_id'   => AnnouncementType::INVESTMENT_ID,
             'user_id'   => User::SYSTEM_USER_ID
         ];
+    }
 
-        return $attributes;
+    protected function createFundingAttributes()
+    {
+        $faker = Faker\Factory::create();
+
+        return [
+            'title'     => 'Pitch Funded',
+            'body'      => $faker->company().' has reached target!',
+            'type_id'   => AnnouncementType::FUNDED_ID,
+            'user_id'   => User::SYSTEM_USER_ID
+        ];
+    }
+
+    protected function createFundingMilestoneAttributes()
+    {
+        $faker = Faker\Factory::create();
+
+        $milestones = [25, 50, 75, 90, 99];
+
+        return [
+            'title'     => $faker->company(),
+            'body'      => 'Milestone reached : '.$milestones[array_rand($milestones)].'% of target!',
+            'type_id'   => AnnouncementType::FUNDING_MILESTONE_ID,
+            'user_id'   => User::SYSTEM_USER_ID
+        ];
+    }
+
+    protected function createBirthdayAttributes()
+    {
+        $faker = Faker\Factory::create();
+
+        $user = $this->createUser();
+
+        return [
+            'title'     => 'Happy Birthday!',
+            'body'      => "It's ".$faker->name."'s Birthday today!",
+            'type_id'   => AnnouncementType::BIRTHDAY_ID,
+            'user_id'   => $user->id
+        ];
+    }
+
+    protected function createCodeDeployAttributes()
+    {
+        $user = $this->createSystemUser();
+
+        return [
+            'title'     => 'Blast Off!',
+            'body'      => 'New application code successfully deployed',
+            'type_id'   => AnnouncementType::CODE_DEPLOY_ID,
+            'user_id'   => $user->id
+        ];
+    }
+
+    protected function createAnnouncementAttributes()
+    {
+        $user = $this->createUser();
+
+        return [
+            'title'     => 'Announcement title',
+            'body'      => 'Body of the announcement',
+            'type_id'   => AnnouncementType::ANNOUNCEMENT_ID,
+            'user_id'   => $user->id
+        ];
+
     }
 }
