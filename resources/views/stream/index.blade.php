9d9e294c6ff16b8d8fbae3bb23240c4354c4dcea
David Ives <david@crowdcube.com>
2017-01-21 19:46:49 +0000

    Bootstrap col classes added to stream items

--- resources/views/stream/index.blade.php
+++ resources/views/stream/index.blade.php
@@ -9,13 +9,17 @@
 @stop
 
 @section('content')
-    <div class="town-crier shouts">
+    <div class="town-crier shouts container">
         @forelse($announcements as $announcement)
             <div class="list-group-item {{ $announcement->type_id }}">
                 <h4 class="shout-title"><span class="glyphicon glyphicon-plus shout-type"></span>{{ $announcement->title }}</h4>
-                <div>
-                    <img src="{{ $announcement->thumb_path }}" alt="No Image Available" />
-                    {{ $announcement->body }}
+                <div class="row">
+                    <div class="col-xs-1">
+                        <img src="{{ $announcement->thumb_path }}" alt="No Image Available" class="stream_thumb" />
+                    </div>
+                    <div class="col-xs-11">
+                        {{ $announcement->body }}
+                    </div>
                 </div>
                 <div class="author">
                     {{ $announcement->user->name }}, {{ $announcement->created_at->diffForHumans() }} <span class="glyphicon glyphicon-time"></span>
