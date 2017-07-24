74d7e4abe41e65dac2beab84b829ee42b0bfb66d
David Ives <chilliweb@gmail.com>
2017-07-24 14:31:29 +0100

    User edit dtail input values tested

--- tests/Browser/EditUserTest.php
+++ tests/Browser/EditUserTest.php
@@ -17,7 +17,6 @@ class EditUserTest extends DuskTestCase
      *
      * @group admin
      * @group user
-     * @group new
      *
      * @return void
      */
@@ -30,6 +29,31 @@ class EditUserTest extends DuskTestCase
                 ->loginAs($user)
                 ->visit('admin/user/2000/edit')
                 ->assertSeeIn('h3.alert-danger', 'User Not Found');
+        });
+    }
+
+    /**
+     * Ensure the correct user details are shown when an edit has been requested
+     *
+     * @group admin
+     * @group user
+     * @group new
+     *
+     * @return void
+     */
+    public function testCorrectUserDetailsAreReturnedWhenEditRequested()
+    {
+        $user       = User::find(User::JANITOR_USER_ID);
+        $edit_user  = User::find(5);
+
+        $this->browse(function ($browser) use ($user, $edit_user) {
+            $browser
+                ->loginAs($user)
+                ->visit('admin/user/'.$edit_user->id.'/edit')
+                ->assertSeeIn('h3', 'Edit '.$edit_user->fullName().'\'s details')
+                ->assertInputValue('first_name', $edit_user->first_name)
+                ->assertInputValue('last_name', $edit_user->last_name)
+                ->assertInputValue('email', $edit_user->email);
         });
     }
 }
